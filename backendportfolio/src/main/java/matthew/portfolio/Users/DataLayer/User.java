507e4b69cd6b6de95ab83bb7d2ddccf6553d00dc
Matthewtan9 <matthew999099@gmail.com>
2024-02-11 17:00:34 -0500

    connected to the database

--- backendportfolio/src/main/java/matthew/portfolio/Users/DataLayer/User.java
+++ backendportfolio/src/main/java/matthew/portfolio/Users/DataLayer/User.java
@@ -18,10 +18,18 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "user_id")
     private String userId;
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "age")
     private String age;
+
+    @Column(name = "email")
     private String email;
 
 
