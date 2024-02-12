8b17af7a1b06d80bbea10aa0cc12369ee9d94ead
Matthewtan9 <matthew999099@gmail.com>
2024-02-12 17:57:58 -0500

    fixing some issue

--- frontendportfolio/src/components/ContactForm.jsx
+++ frontendportfolio/src/components/ContactForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import emailjs from 'emailjs-com';
 import '../css/Form.css';
-
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     name: '',
